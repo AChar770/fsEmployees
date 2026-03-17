b30d7a30b7bb68d116ee97bfebb23135c9cb02db
AChar770 <amandatcharles@yahoo.com>
2026-03-16 23:14:17 -0400

    added middleware and mounted routers in app.js

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 import express from "express";
-const app = express();
-export default app;
+import employeeRouter from "./api.employees.js";
 
-// TODO: this file!
+const app = express();
+
+app.use(express.json())
+
+app.get("/", (req, res) => {
+    res.send("Welcome to the Fs Employees API.");
+});
+
+app.use ("/employees", employeesRouter);
+
+export default app;
